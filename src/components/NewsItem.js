772f9fb0016f8d0d4501304ea0935e695dde1c26
Mohd ZAID KHAN <zaidk9620@gmail.com>
2023-03-04 14:20:10 +0530

    conveted to function based

--- src/components/NewsItem.js
+++ src/components/NewsItem.js
@@ -1,14 +1,17 @@
-import React, { Component } from 'react'
-
-export class NewsItem extends Component {
-    render() {
-        let { title, description, imageUrl, newsUrl, author, date, source } = this.props;
-        return (
-            <div className="my-3">
-                <div className="card">
-                    <div style={{display:'flex',justifyContent:'flex-end', position:'absolute',right:'0'}}>
-                    <span className="badge rounded-pill bg-danger" > {source}
-                    </span>
+import React from 'react'
+const NewsItem = (props) => {
+    let { title, description, imageUrl, newsUrl, author, date, source } = props;
+    return (
+        <div className="my-3">
+            <div className="card">
+                <div style={{
+                    display: 'flex',
+                    justifyContent: 'flex-end',
+                    position: 'absolute',
+                    right: '0'
+                }
+                }>
+                    <span className="badge rounded-pill bg-danger"> {source} </span>
                 </div>
                 <img src={!imageUrl ? "https://fdn.gsmarena.com/imgroot/news/21/08/xiaomi-smart-home-india-annoucnements/-476x249w4/gsmarena_00.jpg" : imageUrl} className="card-img-top" alt="..." />
                 <div className="card-body">
@@ -18,8 +21,9 @@ export class NewsItem extends Component {
                     <a rel="noreferrer" href={newsUrl} target="_blank" className="btn btn-sm btn-dark">Read More</a>
                 </div>
             </div>
-            </div >
-        )
-    }
+        </div>
+    )
+
 }
+
 export default NewsItem
